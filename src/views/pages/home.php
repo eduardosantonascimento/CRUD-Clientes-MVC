4a87b91a7bfb7b83964db076388595fcbf9c0d63
Eduardo Santo <eduardosantonascimento@gmail.com>
2021-08-26 13:43:36 -0300

    CRUD Clientes criado com MVC, todas as funcionalidades funcionando

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,3 +1,32 @@
-<?php $render('header'); ?>
+<?php $render('header');?>
 
-Opa, <?=$nome;?>
+
+
+<a href="<?php echo $base;?>/novo">Novo Usuario</a>
+
+<table border="1" width="100%">
+    <tr>    
+        <th>ID</th>
+        <th>NOME</th>
+        <th>EMAIL</th>
+        <th>AÇÕES</th>
+    </tr>
+    <?php foreach($usuarios as $usuario): ?>
+        <tr>
+            <td><?=$usuario['id'];?></td>
+            <td><?=$usuario['nome'];?></td>
+            <td><?=$usuario['email'];?></td>
+            <td>
+                <a href="<?php echo $base;?>/usuario/<?=$usuario['id'];?>/editar">
+                <img width="20" src="<?=$base;?>/assets/images/document.png"/>
+                </a>
+                <a href="<?php echo $base;?>/usuario/<?=$usuario['id'];?>/excluir" onclick="return confirm('Tem certeza que deseja excluir?')">
+                <img width="20" src="<?=$base;?>/assets/images/trash.png"/>
+                </a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+
+<?php $render('footer');?>
--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -1,1 +1,10 @@
-<h1>Header</h1>
+<html>
+<head>
+    <title>Meu CRUD em MVC</title>
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css">
+</head>
+<body>
+<header>
+    <h1>Titulo do Site</h1>
+    <hr/>
+</header>
